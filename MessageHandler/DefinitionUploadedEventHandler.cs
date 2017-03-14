032aef084a1fe8e08a3964de6498caa7097f1b52
Aidan Twomey <aidantwomey@Aidans-MacBook-Pro.local>
2017-03-14 23:13:37 +0000

    moving to dotnet core 1.1.1

--- MessageHandler/DefinitionUploadedEventHandler.cs
+++ MessageHandler/DefinitionUploadedEventHandler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using System.IO;
+using Amazon.S3.Model;
 
 namespace MessageHandler
 {
@@ -22,25 +23,44 @@ namespace MessageHandler
             this.s3client = s3Client;
             this.createStreamReader = s => streamReader;
         }
+
+        [LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
         public async Task<string> HandleUploadEvent(S3Event evnt, ILambdaContext cont)
         {
+            var logger = cont.Logger;
             var s3Event = evnt.Records[0].S3;
 
-            Console.WriteLine( "bucket {0}, key {1}", s3Event.Bucket.Name, s3Event.Object.Key );
+            logger.LogLine( String.Format( "bucket {0}, key {1}", s3Event.Bucket.Name, s3Event.Object.Key) );
 
-            var response = await this.s3client.GetObjectAsync(s3Event.Bucket.Name, s3Event.Object.Key);
-        
-            using (var stream = response.ResponseStream)
+            var request = new GetObjectRequest(){
+                BucketName = s3Event.Bucket.Name,
+                Key = s3Event.Object.Key,
+                ServerSideEncryptionCustomerMethod = ServerSideEncryptionCustomerMethod.None
+            };
+
+            try
             {
+              logger.LogLine( "Waiting for response" );
+              var response = await s3client.GetObjectAsync(request);
+
+              using (var stream = response.ResponseStream)
+              {
                 TextReader tr = createStreamReader(stream);
                 var s3Document = tr.ReadToEnd();
-                Console.WriteLine("Handler: Got Data from Key");
+                logger.LogLine("Handler: Got Data from Key");
                                 
                 var message = new MessageParser().Parse(s3Document);
 
                 // write this to Dynomo DB
 
                 return message.Name;
+              }
+            }
+            catch ( Exception ex)
+            {
+                logger.LogLine( ex.ToString() + ex.StackTrace );
+
+                return ex.Message;
             }
         }
     }
