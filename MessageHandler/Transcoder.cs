0ca654468072a2f3e417f8e939f080a81f007bbe
aidant <aidan.twomey@7digital.com>
2017-03-20 11:42:01 +0000

    adding transcoder

---
+++ MessageHandler/Transcoder.cs
@@ -0,0 +1,21 @@
+using System;
+// using Amazon.Lambda.Core;
+// using System.Threading.Tasks;
+// using Amazon.Lambda.S3Events;
+// using Amazon.S3;
+// using System.IO;
+// using Amazon.S3.Model;
+using Amazon.ElasticTranscoder.Model;
+
+namespace MessageHandler
+{
+    public class Transcoder
+    {
+        //private readonly ElasticTranscoder transcoder;
+
+        public Transcoder()
+        {
+            //transcoder = new ElasticTranscoder();
+        }
+    }
+}
