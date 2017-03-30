bc4d2bb2313bc8873e3a55bc21d60494e3697e7c
aidant <aidan.twomey@7digital.com>
2017-03-30 12:18:53 +0100

    refactor

--- MessageHandler/TranscodeRequestEventHandler.cs
+++ MessageHandler/TranscodeRequestEventHandler.cs
@@ -2,54 +2,27 @@ using System;
 using Amazon.Lambda.Core;
 using System.Threading.Tasks;
 using Amazon.Lambda.SNSEvents;
-using Amazon;
-using Amazon.ElasticTranscoder.Model;
 using System.Linq;
-// using Amazon.S3;
-// using System.IO;
-// using Amazon.S3.Model;
 
 namespace MessageHandler
 {
     public class TranscodeRequestEventHandler
     {
+        public readonly Transcoder _transcoder;
+
+        public TranscodeRequestEventHandler() 
+        {
+            _transcoder = new Transcoder();
+        }
         
         [LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
         public async Task<string> Transcode(SNSEvent s3event, ILambdaContext context)
         {
             var sourceFile = s3event.Records.First().Sns.Message;
-
+                             
             context.Logger.LogLine( sourceFile );
-
-            var transcoder = new Amazon.ElasticTranscoder.AmazonElasticTranscoderClient(RegionEndpoint.EUWest1);
             
-            var pipelines = await transcoder.ListPipelinesAsync();
-            var pipeline = pipelines.Pipelines.First( p => p.Name == "adex_pipeline");
-
-            pipeline.Role = "arn:aws:iam::160534783289:role/Elastic_Transcoder_Default_Role";
-            pipeline.InputBucket = "myflacs";
-            pipeline.OutputBucket = "mydeflacedfiles";
-
-            context.Logger.LogLine( String.Format("Pipeline Created: {0}, {1}", pipeline.Id, pipeline.Name) );
-
-            var response = await transcoder.CreateJobAsync( new CreateJobRequest(){
-                PipelineId = pipeline.Id,
-                
-                Input = new JobInput()
-                {
-                    Container = "auto",
-                    Key = s3event.Records.First().Sns.Message
-                },
-                Output = new CreateJobOutput()
-                {
-                    Key = String.Format( "{0}.mp3", sourceFile),
-                    PresetId = "1351620000001-300020"
-                }
-            } );
-
-            context.Logger.LogLine( response.HttpStatusCode.ToString() );
-
-            return "transcoded " + sourceFile;
+            return await _transcoder.Transcode(sourceFile, context.Logger);
         }
     }
 }
--- MessageHandler/Transcoder.cs
+++ MessageHandler/Transcoder.cs
@@ -1,5 +1,10 @@
 using Amazon.ElasticTranscoder;
 using Amazon;
+using Amazon.Lambda.Core;
+using Amazon.ElasticTranscoder.Model;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
 
 namespace MessageHandler
 {
@@ -7,14 +12,40 @@ namespace MessageHandler
     {
         private readonly AmazonElasticTranscoderClient transcoder;
 
+        private const string MP3_PRESET =  "1351620000001-300020";
+
         public Transcoder()
         {
             transcoder = new AmazonElasticTranscoderClient(new AmazonElasticTranscoderConfig(){RegionEndpoint = RegionEndpoint.EUWest1 } );
         }
 
-        public void Transcode()
+        public async Task<string> Transcode(string sourceFile, ILambdaLogger logger)
         {
-            //transcoder.CreateJobAsync()
+            var pipelines = await transcoder.ListPipelinesAsync();
+            var pipeline = pipelines.Pipelines.First( p => p.Name == "adex_pipeline");
+
+            logger.LogLine( String.Format("Found pipeline {0} with input {1}, output {2}", pipeline.Name, pipeline.InputBucket, pipeline.OutputBucket));
+
+            pipeline.Role = "arn:aws:iam::160534783289:role/Elastic_Transcoder_Default_Role";
+            
+            var response = await transcoder.CreateJobAsync( new CreateJobRequest(){
+                PipelineId = pipeline.Id,
+                
+                Input = new JobInput()
+                {
+                    Container = "auto",
+                    Key = sourceFile
+                },
+                Output = new CreateJobOutput()
+                {
+                    Key = String.Format( "{0}.mp3", sourceFile),
+                    PresetId = MP3_PRESET
+                }
+            } );
+
+            logger.LogLine( response.HttpStatusCode.ToString() );
+
+            return "transcoded " + sourceFile;
         }
     }
 }
